86b3b99a54b58e61734452b0e88b0011f8e3e6cc
rahul <rahulkandolkar@gmail.com>
2025-09-02 22:24:43 +0530

    feat: add /home and /about endpoinds to the REST controller

--- 01-quick-start/src/main/java/com/lab/quick/start/REST/RESTController.java
+++ 01-quick-start/src/main/java/com/lab/quick/start/REST/RESTController.java
@@ -9,6 +9,11 @@ public class RESTController {
 
     @GetMapping("/home")
     public String home(){
-        return "<h1> welcome home </h1>";
+        return "<h1>home page</h1>";
+    }
+
+    @GetMapping("/about")
+    public String aboout(){
+        return "<h1>about page</h1>";
     }
 }
