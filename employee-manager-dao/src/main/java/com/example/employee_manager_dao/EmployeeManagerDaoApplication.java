6a45bb6a202a14cd67d83f804d0cd3d160c29a1d
rahul <rahulkandolkar@gmail.com>
2025-09-24 13:08:44 +0530

    feat: implement employee entity

---
+++ employee-manager-dao/src/main/java/com/example/employee_manager_dao/EmployeeManagerDaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_manager_dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerDaoApplication.class, args);
+	}
+
+}
