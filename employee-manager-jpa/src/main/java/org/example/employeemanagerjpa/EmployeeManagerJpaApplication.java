e397a196a0632cce6f9dee190f0a16c5be8f1fe8
rahul <rahulkandolkar@gmail.com>
2025-09-26 13:49:53 +0530

    feat: implement entity to map to the databse table

---
+++ employee-manager-jpa/src/main/java/org/example/employeemanagerjpa/EmployeeManagerJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.employeemanagerjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagerJpaApplication.class, args);
+    }
+
+}
---
+++ employee-manager-jpa/src/main/java/org/example/employeemanagerjpa/entity/Employee.java
@@ -0,0 +1,71 @@
+package org.example.employeemanagerjpa.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String last_name;
+
+    @Column(name="department")
+    private String department;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String last_name, String department) {
+        this.firstName = firstName;
+        this.last_name = last_name;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
