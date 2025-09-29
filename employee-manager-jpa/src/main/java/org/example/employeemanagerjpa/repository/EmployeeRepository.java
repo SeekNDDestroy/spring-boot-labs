3a98c19d8e610209e23d455d753d48a4b4bf4c7a
SeekNDDestroy <104748596+SeekNDDestroy@users.noreply.github.com>
2025-09-29 14:26:57 +0530

    Delete employee-manager-jpa directory

--- employee-manager-jpa/src/main/java/org/example/employeemanagerjpa/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.employeemanagerjpa.repository;
-
-import org.example.employeemanagerjpa.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    
-}
