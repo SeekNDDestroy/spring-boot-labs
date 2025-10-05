642fe22d46a3dccabbc6b7a41642b2b18d02aadd
rahul <rahulkandolkar@gmail.com>
2025-10-05 22:53:18 +0530

    chore(thymeleaf): add SpringMVCController stub and minor template/css changes

--- 08-thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/SpringMVCController.java
+++ 08-thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/SpringMVCController.java
@@ -1,4 +1,18 @@
 package com.example.thymeleafdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
 public class SpringMVCController {
+
+
+    @GetMapping("/hello")
+    public String sayHello(Model model){
+        model.addAttribute("date", java.time.LocalDateTime.now());
+        return "helloworld";
+    }
 }
