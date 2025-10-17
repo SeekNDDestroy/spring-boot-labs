bb3a6a08c1c09ca5e713990ecb97a100e7b2b3fd
rahul <rahulkandolkar@gmail.com>
2025-10-17 10:53:33 +0530

    updates

--- 07-SpringSecurityDemo-JDBC authentication/src/main/java/org/example/springsecuritydemo/security/SecurityConfig.java
+++ 07-SpringSecurityDemo-JDBC authentication/src/main/java/org/example/springsecuritydemo/security/SecurityConfig.java
@@ -8,11 +8,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
 
     @Bean
